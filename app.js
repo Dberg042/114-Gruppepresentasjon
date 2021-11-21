d9839a65d005129151423c9600339b2e1b0eb726
Dberg042 <62940443+Dberg042@users.noreply.github.com>
2021-11-21 11:42:45 +0100

    fikset comment

--- app.js
+++ app.js
@@ -4,21 +4,18 @@ const img2 = document.querySelector('#photo2');
 const file = document.querySelector('#file');
 const uploadBtn = document.querySelector('#uploadBtn');
 
-//if user hover on img div
+
 
 imgDiv.addEventListener('mouseenter', function(){
     uploadBtn.style.display = "block";
 });
 
-//if we hover out from img div
 
 imgDiv.addEventListener('mouseleave', function(){
     uploadBtn.style.display = "none";
 });
 
-//lets work for image showing functionality when we choose an image to upload
 
-//when we choose a foto to upload
 
 file.addEventListener('change', function(){
     //this refers to file
@@ -35,14 +32,6 @@ file.addEventListener('change', function(){
 
         reader.readAsDataURL(choosedFile);
 
-        //Allright is done
-
-        //please like the video
-        //comment if have any issue related to vide & also rate my work in comment section
-
-        //And aslo please subscribe for more tutorial like this
-
-        //thanks for watching
-    }
+     }
 });
 
